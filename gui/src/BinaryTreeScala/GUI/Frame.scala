b7e2452bd1192bd474b8fafc2035690970acfd54
Alexey Lykov <aleksey.lykov@eltex.loc>
2017-12-19 10:14:40 +0700

    Layout fix

--- gui/src/BinaryTreeScala/GUI/Frame.scala
+++ gui/src/BinaryTreeScala/GUI/Frame.scala
@@ -8,7 +8,9 @@ class Frame extends MainFrame {
   private var tree:BTree[Int] = new BTree[Int]((k1,k2) => k1.compareTo(k2))
   val nodeField = new TextField(columns = 1)
   val output = new TextArea()
+  val controlsPanel = new GridPanel(2,1)
   val buttonsPanel = new GridPanel(1,4)
+  preferredSize = new Dimension(500,400)
   buttonsPanel.contents += Button("Add node") {
     try {
       tree.add(nodeField.text.toInt)
@@ -32,12 +34,13 @@ class Frame extends MainFrame {
     nodeField.text = ""
   }
   buttonsPanel.contents += nodeField
+  controlsPanel.contents += buttonsPanel
+  controlsPanel.contents += Button("Show tree") {
+    output.text = tree.show()
+  }
   contents = new BorderPanel{
-    add(Button("Show tree") {
-      output.text = tree.show()
-    },BorderPanel.Position.South)
-    add(buttonsPanel,BorderPanel.Position.Center)
-    add(output,BorderPanel.Position.apply(0))
+    add(controlsPanel,BorderPanel.Position.South)
+    add(output,BorderPanel.Position.Center)
   }
 
 }
